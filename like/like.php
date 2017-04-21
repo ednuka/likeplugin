b44418a82e08547e7fc94e2538c7ff296d6e84a5
Edibe Nur <edibenurkarayel@gmail.com>
2017-04-21 18:08:04 +0300

    Add files via upload

---
+++ like/like.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+  Plugin Name: Like Plugin
+  Plugin URI: http://localhost/wordpresss
+  Description: Eklenti Açıklaması
+  Version: Versiyon (0.1 gibi)
+  Author: Ednuka
+  Author URI:  http://localhost/wordpresss
+  License: GNU
+ */
+
+
+
+/* Direkt Çağrıları Engelleyelim */
+if (preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) {
+    die('You are not allowed to call this page directly.');
+}
+
+function like_form() {
+   
+    $form = "<form action=''>"
+            . "<input type='text' name=''/>"
+            . "<button>Begen1</button>"
+            . "</form>";
+    return $form;
+}
+
+function like_btn($content) {
+
+    
+
+    if (is_single()) {
+        $buton = like_form();
+        $content .= $buton;
+    }
+    return $content;
+}
+
+add_filter('the_content', 'like_btn');
+?>
